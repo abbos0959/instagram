7bdd18c90a4fb983d573305daa9c0aedd651b78c
abbos0959 <abbosakromovich777@gmail.com>
2022-03-28 00:25:33 +0500

    boshladik

---
+++ src/Post.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./post.css"
+import Avatar from "@material-ui/core/Avatar"
+export const Post = ({imageUrl,username}) => {
+  return (
+    <div className='post'>
+        <div className='post_header'>
+        <Avatar 
+        className='post_avatar'
+        src=''
+        />
+        
+        <h3>{username}</h3>
+        </div>
+        <img className='post_image' src={imageUrl}/>
+    
+    <h4 className='post_text'><strong>{username}</strong> :Caption</h4>
+    </div>
+  )
+}
